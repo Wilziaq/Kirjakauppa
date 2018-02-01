f12dc65c127d40acbc7cbfef72e2328b19ce0dee
Viltsu <vpalmgren@gmail.com>
2018-02-01 12:36:32 +0200

    h3t3

--- Kirjakauppa/bookcrap/src/main/java/com/example/bookcrap/KirjaKontrolleri/KirjaController.java
+++ Kirjakauppa/bookcrap/src/main/java/com/example/bookcrap/KirjaKontrolleri/KirjaController.java
@@ -3,7 +3,10 @@ package com.example.bookcrap.KirjaKontrolleri;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.ArrayList;
 
@@ -13,14 +16,32 @@ public class KirjaController {
     private KirjaRepo repo;
     private ArrayList<Kirja> kirja = new ArrayList<Kirja>();
 
-    @RequestMapping(value="/index")
+    @RequestMapping(value = "/index")
     public String home(){
         return "index";
     }
+
     //tehtävä 3 harjoitus 2 osiota varten kaikki kirjat:
-    @RequestMapping(value="/lista")
+    @RequestMapping(value = "/lista")
     public String listAll(Model model){
         model.addAttribute("lista", repo.findAll());
         return "booklist";
+    }
+    //Tehtävä 3 harjoitus 3 osiota varten Create ja Delete toiminnot sivulle:
+    @RequestMapping(value = "/addbook")
+    public String luoFormi(Model model){
+        model.addAttribute("kirja", new Kirja());
+        return "addbook";
+    }
+    @RequestMapping(value = "/save", method= RequestMethod.POST)
+    public String lisaaKirja(Kirja kirja){
+        repo.save(kirja);
+        return "redirect:/lista";
+    }
+    @RequestMapping(value = "/delete/{id}", method = RequestMethod.GET)
+    public String deleteBook(@PathVariable("id") String id, Model model){
+        Long ID = Long.parseLong(id);
+        repo.delete(ID);
+        return "redirect:/lista";
     }
 }
